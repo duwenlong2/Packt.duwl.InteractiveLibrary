9ded7e877918b8aaf368f13999c3b50b4d485f3b
duwl2 <duwl2@lenovo.com>
2023-01-06 00:53:07 +0800

    Add project files.

---
+++ Packt.duwl.InteractiveLibrary/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace Packt.duwl.InteractiveLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool IsValidPassword(this string input)
+        {
+            return Regex.IsMatch(input, "^[a-zA-Z0-9_-]{8,}$");
+        }
+    }
+}
